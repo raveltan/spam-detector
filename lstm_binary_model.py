c0c0487cb55241efe9c17cd6e31040bfb8105182
dankpanda <yowenyo17@gmail.com>
2021-06-23 23:54:53 +0700

    adding missing import

--- lstm_binary_model.py
+++ lstm_binary_model.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.utils import class_weight
 
